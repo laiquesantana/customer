460242569e2a4b7717043603d776c3729e0401f7
laique <laiquesantana@gmail.com>
2024-09-14 15:43:03 -0300

    update docker file with gd extension, update db config to use env variables and, update docker composer yaml to allows app container access variables

--- Dockerfile
+++ Dockerfile
@@ -16,6 +16,7 @@ RUN apt-get update && apt-get install -y \
     libzip-dev \
     libpq-dev
 
+RUN docker-php-ext-install gd
 # Install PHP extensions
 RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd zip
 
--- config/params.php
+++ config/params.php
@@ -4,4 +4,8 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'jwtSecret' => getenv('JWT_SECRET_KEY') ?: 'your-secret-key',
+    'jwtIssuer' => getenv('JWT_ISSUER') ?: 'http://localhost',
+    'jwtAudience' => getenv('JWT_AUDIENCE') ?: 'http://localhost',
+    'jwtExpiry' => 3600, // Token validity in seconds
 ];
